0b35bcdfe6326fa9e4133b39f9f7cca78a62bcb3
lifeonhoth <fhbach@gmail.com>
2016-12-24 00:03:21 -0500

    adding comments, breaking out probe power

--- soil_moisture_sensor.ino
+++ soil_moisture_sensor.ino
@@ -3,24 +3,26 @@
   Fletcher Bach | Dec. 2016
   
   Connect the Soil Moisture Sensor to anolog input pin 0
+  Connect sensor's power to digital pin 7 on the Arduino
 */
 
 //const long intervalDuration = 1800000; // wait half an hour = 1800000
+const int probePowerPin = 7;
 
 void setup() {
 
   // Serial Begin so we can see the data from the sensor in our serial monitor.
   Serial.begin(9600);
+  pinMode(probePowerPin, OUTPUT);
 
 }
 
 void loop() {
-  poll(0, 7, 1000);
+  poll(0, 1000);
 }
 
-void poll(int whichPin, int probePowerPin, long intervalDuration){
+void poll(int whichPin, long intervalDuration){
    // Power-up the probe
-  pinMode(probePowerPin, OUTPUT);
   digitalWrite(probePowerPin, HIGH);
   delay(1000);
   
